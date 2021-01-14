7a1b55cdd38cabd8dc5490c80891be40a12b35db
Kazuki Inamura <kzinmr109@gmail.com>
2021-01-14 22:35:14 +0900

    fix

--- ner_trf.py
+++ ner_trf.py
@@ -200,6 +200,7 @@ def main():
         # Downloading and loading a dataset from the hub.
         datasets = load_dataset(data_args.dataset_name, data_args.dataset_config_name)
     else:
+        import csv
         data_files = {}
         if data_args.train_file is not None:
             data_files["train"] = data_args.train_file
@@ -208,7 +209,7 @@ def main():
         if data_args.test_file is not None:
             data_files["test"] = data_args.test_file
         extension = data_args.train_file.split(".")[-1]
-        datasets = load_dataset(extension, data_files=data_files, delimiter='\t')  # .tsv
+        datasets = load_dataset(extension, data_files=data_files, delimiter='\t', quoting=csv.QUOTE_NONE, encoding='utf-8', header=None)   # .tsv
     # See more about loading any type of standard or custom dataset (from files, python dict, pandas DataFrame, etc) at
     # https://huggingface.co/docs/datasets/loading_datasets.html.
 
